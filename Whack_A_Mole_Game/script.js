a3c1678a92da10e832bc9d3542392c73788577a6
Arun Kumar <kumar05.962013@gmail.com>
2020-10-04 08:06:42 +0530

    complete

--- Whack_A_Mole_Game/script.js
+++ Whack_A_Mole_Game/script.js
@@ -2,15 +2,19 @@ const startbtn = document.getElementById("startbtn");
 const container = document.getElementById("container");
 
 const holes = document.querySelectorAll(".hole");
-const scoreBoard = document.querySelectorAll(".score");
+const scoreBoard = document.querySelector("#score");
 const moles = document.querySelectorAll(".mole");
+const gameOver = document.getElementById("gameOver");
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 startbtn.addEventListener("click", () => {
   container.classList.add("show");
+  gameOver.style.display = "none";
 });
 
-function randTime(min, max) {
+function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
@@ -23,3 +27,39 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function gameEnd() {
+  container.classList.remove("show");
+  gameOver.style.display = "block";
+  scoreBoard.style.display = "inline";
+}
+
+function startGame() {
+  scoreBoard.innerText = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => {
+    timeUp = true;
+    gameEnd();
+  }, 10000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.innerText = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
